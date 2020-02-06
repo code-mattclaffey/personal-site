4549a53b98ea2605a6126dce399709eeb9a564aa
Matt Claffey <mattclaffey93@gmail.com>
2020-02-06 14:57:07 +0000

    test: see if this loads fonts faster

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,25 @@ const Home = () => (
         <Head>
             <title>Home</title>
             <link rel="icon" href="/favicon.ico" />
-            <link rel="preload" href="/GothaProBol.woff2" as="font" type="font/woff2" crossorigin></link>
-            <link rel="preload" href="/GothaProLig.woff2" as="font" type="font/woff2" crossorigin></link>
+            <style jsx>
+                {`
+                    @font-face {
+                        font-display: swap;
+                        font-family: 'Gotham';
+                        font-style: normal;
+                        font-weight: 700;
+                        src: url('/GothaProBol.woff2') format('woff2');
+                    }
+
+                    @font-face {
+                        font-display: swap;
+                        font-family: 'Gotham';
+                        font-style: normal;
+                        font-weight: 300;
+                        src: url('/GothaProLig.woff2') format('woff2');
+                    }
+                `}
+            </style>
         </Head>
         <Header />
         <Banner />
