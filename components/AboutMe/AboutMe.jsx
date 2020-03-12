f0f7f39c4c03795d86bd5de6f37a73ed4a24451d
Matt Claffey <mattclaffey93@gmail.com>
2020-03-12 17:43:02 +0000

    feat: update font sizes on desktop to bring more meaning to content

--- components/AboutMe/AboutMe.jsx
+++ components/AboutMe/AboutMe.jsx
@@ -20,10 +20,10 @@ export const AboutMe = () => {
                     <Grid>
                         <GridItem columns={{ xs: '12', md: '9' }}>
                             <div>
-                                <Heading type="h4" additionalClassNames="c-about-me__main-heading">
+                                <Heading type="h3" additionalClassNames="c-about-me__main-heading">
                                     Who am I?
                                 </Heading>
-                                <Text additionalClassNames="u-text--larger">
+                                <Text additionalClassNames="u-text--super">
                                     My focus at the moment is
                                     building reusable design systems,{' '}
                                     <abbr title="Continous Integration">
@@ -43,13 +43,33 @@ export const AboutMe = () => {
                                 </Text>
                             </div>
                         </GridItem>
-                        <GridItem columns={{ xs: '12', lg: '7' }}>
+                        <GridItem columns={{ xs: '12', lg: '6' }}>
                             <div className="c-card">
-                            <Heading as="h4" type="h5">
+                            <Heading as="h3" type="h4">
+                                Frontend Developer
+                            </Heading>
+                            <Text additionalClassNames="u-text--larger@md">
+                                Making code as reusable as
+                                possible so I spend a lot of time
+                                building tools that make my day to day
+                                a lot faster.
+                            </Text>
+                            <Text additionalClassNames="u-text--larger@md">
+                                The languages I am
+                                mainly using at the minute are HTML,
+                                CSS, React, PHP, JavaScript. Other
+                                languages I am familliar with are .NET
+                                & NodeJS.
+                            </Text>
+                            </div>
+                        </GridItem>
+                        <GridItem columns={{ xs: '12', lg: '6' }}>
+                            <div className="c-card">
+                            <Heading as="h3" type="h4">
                                 Designer
                             </Heading>
-                            <Text>
-                                I love designing in the browser
+                            <Text additionalClassNames="u-text--larger@md">
+                                Designing in the browser
                                 whenever I can so I can see my project
                                 come to life! I work mainly in Sketch
                                 & Photoshop but I have used before;
@@ -66,103 +86,79 @@ export const AboutMe = () => {
                             </Text>
                             </div>
                         </GridItem>
-                        <GridItem columns={{ xs: '12', lg: '7' }}>
+                        <GridItem columns={{ xs: '12', lg: '6' }}>
                             <div className="c-card">
-                            <Heading as="h4" type="h5">
-                                Frontend Developer
-                            </Heading>
-                            <Text>
-                                I love making things as reusable as
-                                possible so I spend a lot of time
-                                building tools that make my day to day
-                                a lot faster.
-                            </Text>
-                            <Text>
-                                The languages I am
-                                mainly using at the minute are HTML,
-                                CSS, React, PHP, JavaScript. Other
-                                languages I am familliar with are .NET
-                                & NodeJS.
-                            </Text>
+                                <Heading as="h4" type="h5">
+                                    Speaker
+                                </Heading>
+                                <Text>
+                                    Once a year I talk at an event called
+                                    MCR Fred on something I am passionate
+                                    about. It normally sparks from me
+                                    doing talks or running workshops in
+                                    the workplace and then branching it
+                                    out into the frontend community.
+                                </Text>
+                                <Text>
+                                    If I
+                                    feel like it will help someone then I
+                                    will do it! I also write a lot of
+                                    blogs on Medium too.
+                                </Text>
+                            </div>
+                            <div className="c-card">
+                                <Heading as="h4" type="h5">
+                                    Tooling
+                                </Heading>
+                                <Text>
+                                    I have been focusing my efforts mainly
+                                    into{' '}
+                                    <abbr title="Continous Integration">
+                                        CI
+                                    </abbr>{' '}
+                                    at the minute which is mainly around
+                                    using tools such as Netlify &
+                                    TravisCI. I have worked with in the
+                                    past Jenkins, AWS Code Pipeline &
+                                    TeamCity.
+                                </Text>
                             </div>
                         </GridItem>
-                        <GridItem columns={{ xs: '12', lg: '7' }}>
+                        <GridItem columns={{ xs: '12', lg: '6' }}>
                             <div className="c-card">
-                            <Heading as="h4" type="h5">
-                                Speaker
-                            </Heading>
-                            <Text>
-                                Once a year I talk at an event called
-                                MCR Fred on something I am passionate
-                                about. It normally sparks from me
-                                doing talks or running workshops in
-                                the workplace and then branching it
-                                out into the frontend community.
-                            </Text>
-                            <Text>
-                                If I
-                                feel like it will help someone then I
-                                will do it! I also write a lot of
-                                blogs on Medium too.
-                            </Text>
+                                <Heading as="h4" type="h5">
+                                    Content Management Systems
+                                </Heading>
+                                <Text>
+                                    Platforms I am working in are Magento
+                                    however I have expierence in working
+                                    in Umbraco, Sitecore, Contentful &
+                                    Netlify CMS. Trying to work on
+                                    projects that are more headless now so
+                                    working on GraphQl, Apollo Client and
+                                    NextJS
+                                </Text>
                             </div>
-                        </GridItem>
-                        <GridItem columns={{ xs: '12', lg: '7' }}>
                             <div className="c-card">
-                            <Heading as="h4" type="h5">
-                                Tooling
-                            </Heading>
-                            <Text>
-                                I have been focusing my efforts mainly
-                                into{' '}
-                                <abbr title="Continous Integration">
-                                    CI
-                                </abbr>{' '}
-                                at the minute which is mainly around
-                                using tools such as Netlify &
-                                TravisCI. I have worked with in the
-                                past Jenkins, AWS Code Pipeline &
-                                TeamCity.
-                            </Text>
-                            </div>
-                        </GridItem>
-                        <GridItem columns={{ xs: '12', lg: '7' }}>
-                            <div className="c-card">
-                            <Heading as="h4" type="h5">
-                                Content Management Systems
-                            </Heading>
-                            <Text>
-                                Platforms I am working in are Magento
-                                however I have expierence in working
-                                in Umbraco, Sitecore, Contentful &
-                                Netlify CMS. Trying to work on
-                                projects that are more headless now so
-                                working on GraphQl, Apollo Client and
-                                NextJS
-                            </Text>
-                            </div>
-                        </GridItem>
-                        <GridItem columns={{ xs: '12', lg: '7' }}>
-                            <div className="c-card">
-                            <Heading as="h4" type="h5">
-                                Events
-                            </Heading>
-                            <Text>
-                                One of my goals in life is to travel
-                                and visit a lot of places before I
-                                settle down so I decided to invest in
-                                travelling to see conferences all over
-                                the world. 
-                            </Text>
-                            <Text>
-                                Events I have attended are;
-                                2 x Google Hackathons (London), DotCSS
-                                & DotJS (Paris), MCR Fred
-                                (Manchester), All Day Hey (Leeds) &
-                                UpFront North (Manchester). I need to
-                                get planning for another one or two
-                                this year!
-                            </Text>
+                                <Heading as="h4" type="h5">
+                                    Events
+                                </Heading>
+                                <Text>
+                                    One of my goals in life is to travel
+                                    and visit a lot of places before I
+                                    settle down so I decided to invest in
+                                    travelling to see conferences all over
+                                    the world. 
+                                </Text>
+                                <Text>
+                                    Events I have attended are;
+                                    2 x Google Hackathons (London), DotCSS
+                                    & DotJS (Paris), MCR Fred
+                                    (Manchester), All Day Hey (Leeds) &
+                                    UpFront North (Manchester). I need to
+                                    get planning for another one or two
+                                    this year!
+                                </Text>
                             </div>
                         </GridItem>
                     </Grid>
