437bdfb8645b78d9539d97c5d56f27676be46f81
Matt Claffey <mattclaffey93@gmail.com>
2020-03-14 20:23:29 +0000

    feat: add service worker to website

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withCSS = require('@zeit/next-css');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const withOffline = require('next-offline')
 
-module.exports = withBundleAnalyzer(withCSS({
-  /* config options here */
-}));
+
+module.exports = withOffline(withBundleAnalyzer(withCSS()));
