79a382e6a874e019bb53c12bccc04ba342a6724f
Matt Claffey <mattclaffey93@gmail.com>
2020-03-14 20:08:00 +0000

    chore: update seo and look into bundle analzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 const withCSS = require('@zeit/next-css');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = withCSS({
+module.exports = withBundleAnalyzer(withCSS({
   /* config options here */
-})
+}));
--- pages/_document.js
+++ pages/_document.js
@@ -14,70 +14,7 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang="en">
-                <Head>
-                    <title>
-                        Matt Claffey - Manchester Based - Senior Frontend
-                        Developer
-                    </title>
-                    <meta
-                        name="description"
-                        content="Hi I'm Matt, a senior frontend developer based in Manchester."
-                    />
-                    <link rel="icon" href="/favicon.ico" />
-                    <meta
-                        property="og:title"
-                        content="Matt Claffey - Manchester Based - Senior Frontend Developer"
-                    />
-                    <meta
-                        property="og:description"
-                        content="Hi I'm Matt, I have been a frontend developer for
-                        around 6 years, I've done work for multiple agencies,
-                        consulted for clients, and collaborated with talented
-                        people to create digital products."
-                    />
-                    <meta
-                        property="og:image"
-                        content="https://mattclaffey.netlify.com/thumbnail.jpg"
-                    />
-                    <meta
-                        property="og:url"
-                        content="https://mattclaffey.netlify.com"
-                    />
-                    <meta
-                        name="twitter:title"
-                        content="Matt Claffey - Manchester Based - Senior Frontend Developer"
-                    />
-                    <meta
-                        name="twitter:description"
-                        content=" Hi I'm Matt, I have been a frontend developer for
-                        around 6 years, I've done work for multiple agencies,
-                        consulted for clients, and collaborated with talented
-                        people to create digital products."
-                    />
-                    <meta
-                        name="twitter:image"
-                        content="https://mattclaffey.netlify.com/thumbnail.jpg"
-                    />
-                    <meta name="twitter:card" content="summary_large_image" />
-
-                    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-                    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-                    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-                    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-                    <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-                    <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-                    <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-                    <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-                    <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-                    <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png" />
-                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                    <link rel="manifest" href="/manifest.json" />
-                    <meta name="msapplication-TileColor" content="#151a3c"  />
-                    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-                    <meta name="theme-color" content="#151a3c"  />
-                </Head>
+                <Head />
                 <body>
                     <Main />
                     <NextScript />
