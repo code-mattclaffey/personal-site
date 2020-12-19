02dfa23f6d2f6ee07ccb06f480f9c3f8da7ad855
code-mattclaffey <mattclaffey93@gmail.com>
2020-12-19 13:11:14 +0000

    feat: get rid of a lot fo stuff I do not need

--- src/site/_data/banner.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  bannerTitle: '👨🏻‍💻 Designer, Frontend Developer & Speaker living in Manchester.',
-  bannerDesc: `
-        Hi! I'm Matt, I specialise in in HTML5, CSS3, JavaScript/React and have experience working with .NET &amp; PHP Platforms. I am currently working as a Software Engineer for a company called
-        <a href="https://cinch.co.uk/" target="_blank" rel="noreferrer">Cinch</a>, who are based in Manchester.
-    `,
-};
