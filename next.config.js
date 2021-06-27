7dba1db451a009c0f5d0a89403f015fa80957416
code-mattclaffey <mattclaffey93@gmail.com>
2021-06-27 21:44:26 +0100

    chore: remove next offline

--- next.config.js
+++ next.config.js
@@ -1,20 +1,2 @@
-const withOffline = require('next-offline')
 const withImages = require('next-images')
-
-const nextConfig = {
-  workboxOpts: {
-    urlPattern: /^https?.*/,
-    handler: 'NetworkFirst',
-    options: {
-      cacheName: 'offlineCache',
-      expiration: {
-        maxEntries: 200
-      },
-      cacheableResponse: {
-        statuses: [0, 200],
-      }
-    }
-  }
-}
-
 module.exports = withImages()
