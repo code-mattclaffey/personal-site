bf57638e5eac1ed4c153bb4cdab1c9d537c51baf
code-mattclaffey <mattclaffey93@gmail.com>
2022-07-08 19:00:12 +0100

    Fix banner by moving styles into the react component so when hydration happens the styles are included

--- src/components/banner/banner.component.tsx
+++ src/components/banner/banner.component.tsx
@@ -1,11 +1,6 @@
 import Link from "next/link";
 import React from "react";
 
-const styles = {
-  '--region-background': 'var(--base-region-bg);',
-  '--region-color': 'var(--color-white);',
-};
-
 export interface BannerProps {
   title: string;
   link?: {
@@ -14,16 +9,23 @@ export interface BannerProps {
   }
 }
 
-export const Banner: React.FC<BannerProps> = ({ title, children, link }) => (
-  <section className="o-region o-region--py-large" style={styles as any}>
-    <div className="o-region__inner">
-      {link && (
-        <Link href={link.to}>
-          <a className="c-link c-link--alt">{link.text}</a>
-        </Link>
-      )}
-      <h1 className="e-heading e-heading--h1 c-banner__title">{title}</h1>
-      {children}
-    </div>
-  </section>
-)
+export const Banner: React.FC<BannerProps> = ({ title, children, link }) => {
+  const styles = {
+    '--region-background': 'var(--base-region-bg);',
+    '--region-color': 'var(--color-white);',
+  };
+
+  return (
+    <section className="o-region o-region--py-large" style={styles as any}>
+      <div className="o-region__inner">
+        {link && (
+          <Link href={link.to}>
+            <a className="c-link c-link--alt">{link.text}</a>
+          </Link>
+        )}
+        <h1 className="e-heading e-heading--h1 c-banner__title">{title}</h1>
+        {children}
+      </div>
+    </section>
+  )
+}
